8dcc1b6b1fe56376b990dd0f798269fee829a02f
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-25 18:46:10 +0200

    [upd] migration file for cover column

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -3,7 +3,20 @@
 {{-- create categories --}}
 
 @section('content')
-    <h3 class="text-white">crea nuova categoria</h3>
+    <h3 class="text-white text-uppercase">crea nuova categoria</h3>
+
+    <div class="col-md-12">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+
     <form action="{{route('admin.categories.store')}}" method="POST">
         @csrf
         <div class="form-group">
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -14,7 +14,7 @@
             </div>
         @endif
         
-        <form action="{{ route('admin.projects.store') }}" method="POST">
+        <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="row">
                 <div class="col-md-6">
